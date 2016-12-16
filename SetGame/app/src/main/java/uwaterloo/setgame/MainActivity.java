afb7fca4f9a16a174530791857e7d1d3bfd6fbd6
Stephen Cholvat <stephen.cholvat@gmail.com>
2016-12-15 21:09:00 -0500

    Got Camera on MainActivity
    Put Camera on MainActivity Screen

--- SetGame/app/src/main/java/uwaterloo/setgame/MainActivity.java
+++ SetGame/app/src/main/java/uwaterloo/setgame/MainActivity.java
@@ -3,23 +3,48 @@ package uwaterloo.setgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 import java.util.ArrayList;
 
 import uwaterloo.setgame.util.Card;
 import uwaterloo.setgame.util.Deck;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements CameraBridgeViewBase.CvCameraViewListener2{
 
     private static final String TAG="MainActivity";
 
+    Mat mRgba;
+
+    JavaCameraView javaCameraView;
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:
+                    javaCameraView.enableView();
+                break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+            super.onManagerConnected(status);
+        }
+    };
+
     static{
         if(OpenCVLoader.initDebug()){
-            Log.d(TAG,"OpenCV Successfully Loaded");
+            Log.i(TAG,"OpenCV Successfully Loaded");
         }else{
-            Log.d(TAG,"OpenCV Failed to Load");
+            Log.i(TAG,"OpenCV Failed to Load");
         }
     }
     @Override
@@ -27,11 +52,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Deck deck = new Deck();
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+
+
+        /*Deck deck = new Deck();
         deck.shuffle();
         ArrayList<Card> cards = deck.getCards();
         for(Card c : cards){
             //Log.d(TAG,c.toString());
+        }*/
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView!=null){
+            javaCameraView.disableView();
         }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView!=null){
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.i(TAG,"OpenCV Successfully Reloaded onResume");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else{
+            Log.i(TAG,"OpenCV Failed to reload onResume");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height,width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
     }
 }
