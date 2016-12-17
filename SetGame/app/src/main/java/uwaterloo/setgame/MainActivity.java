3667407b58f6dfd6f4dc5643daa5e4380efd4701
Stephen Cholvat <stephen.cholvat@gmail.com>
2016-12-17 17:15:24 -0500

    Tried contours?
    Tried adding contours, it is now printing to imgContours but can't do
    anything with it yet.

--- SetGame/app/src/main/java/uwaterloo/setgame/MainActivity.java
+++ SetGame/app/src/main/java/uwaterloo/setgame/MainActivity.java
@@ -7,8 +7,10 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -16,6 +18,7 @@ import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
+import org.opencv.core.CvException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity  implements CameraBridgeView
 
     private static final String TAG="MainActivity";
 
-    Mat mRgba, imgGray, imgBlur,imgCanny, imgHSV,imgHSVThreshold, imgDilate;
+    Mat mRgba, imgGray, imgBlur,imgCanny, imgHSV,imgContours, imgDilate;
 
     //debug stuff
     Spinner cameraView;
@@ -72,13 +75,12 @@ public class MainActivity extends AppCompatActivity  implements CameraBridgeView
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
 
-
         //debug spinner to change filters
         cameraView = (Spinner) findViewById(R.id.camera_view_spinner);
         String[] arraySpinner = new String[] {
                 "Normal", "Black and White", "Canny", "HSV","Blur","Dilate"
         };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
         cameraView.setAdapter(adapter);
 
@@ -136,7 +138,7 @@ public class MainActivity extends AppCompatActivity  implements CameraBridgeView
         imgBlur = new Mat(height,width, CvType.CV_8UC1);
         imgCanny = new Mat(height,width, CvType.CV_8UC1);
         imgHSV = new Mat(height,width, CvType.CV_8UC3);
-        imgHSVThreshold = new Mat(height,width, CvType.CV_8UC3);
+        imgContours  = new Mat(height,width, CvType.CV_8UC3);
         imgDilate = new Mat(height,width, CvType.CV_8UC1);
     }
 
@@ -147,44 +149,65 @@ public class MainActivity extends AppCompatActivity  implements CameraBridgeView
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        //Get RGBA Camera Image
         mRgba = inputFrame.rgba();
 
-        Imgproc.cvtColor(mRgba,imgGray,Imgproc.COLOR_RGB2GRAY);
-
-        Imgproc.cvtColor(mRgba,imgHSV,Imgproc.COLOR_RGB2HSV);
-
         try {
-            Core.inRange(imgHSV, new Scalar(Integer.valueOf(b1.getText().toString()), Integer.valueOf(b2.getText().toString()), Integer.valueOf(b3.getText().toString()))
-                    , new Scalar(Integer.valueOf(t1.getText().toString()), Integer.valueOf(t2.getText().toString()), Integer.valueOf(t3.getText().toString())), imgHSVThreshold);
+            //Convert to Grayscale
+            Imgproc.cvtColor(mRgba,imgGray,Imgproc.COLOR_RGB2GRAY);
+
+            //Imgproc.cvtColor(mRgba,imgHSV,Imgproc.COLOR_RGB2HSV);
+
+            //Core.inRange(imgHSV, new Scalar(Integer.valueOf(b1.getText().toString()), Integer.valueOf(b2.getText().toString()), Integer.valueOf(b3.getText().toString()))
+                    //, new Scalar(Integer.valueOf(t1.getText().toString()), Integer.valueOf(t2.getText().toString()), Integer.valueOf(t3.getText().toString())), imgHSVThreshold);
+
+            //Blur Image
             Imgproc.GaussianBlur(imgGray, imgBlur, new Size(Integer.valueOf(b1.getText().toString()),Integer.valueOf(b1.getText().toString())),0);
+
+            //Canny Image
             Imgproc.Canny(imgBlur,imgCanny,50,150);
 
-            Imgproc.dilate(imgCanny,imgDilate,new Mat());
-
-            List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+            //Find Contours
+            List<MatOfPoint> contours = new ArrayList<>();
             Imgproc.findContours(imgCanny,contours,new Mat(),Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Log.d(TAG,"Size of contours: " + String.valueOf(contours.size()));
+
+            //Draw contours
+            imgContours  = new Mat(imgContours.height(),imgContours.width(), CvType.CV_8UC3);
+            Imgproc.drawContours(imgContours, contours, -1, new Scalar(255, 255, 255), -1);
+
+            Mat element = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,new Size(2, 2));
 
 
             //debug spinner to change filters
             String cameraFilter = cameraView.getSelectedItem().toString();
             if(cameraFilter.compareTo("Normal")==0){
+                Log.d(TAG,"RGBA");
                 return mRgba;
             }else if(cameraFilter.compareTo("Black and White")==0){
+                Log.d(TAG,"Gray");
                 return imgGray;
             }else if(cameraFilter.compareTo("HSV")==0){
-                return imgHSVThreshold;
+                Log.d(TAG,"HSV");
+                return imgContours;
             }else if(cameraFilter.compareTo("Blur")==0){
+                Log.d(TAG,"Blur");
                 return imgBlur;
             }else if(cameraFilter.compareTo("Dilate")==0){
-            return imgDilate;
+                Log.d(TAG,"dilate");
+            return element;
             }
             else{
                 return imgCanny;
             }
-        }catch(Exception e){
+        }catch(NumberFormatException e){
             Log.e(TAG,e.toString());
-            return mRgba;
+
         }
+        catch(CvException e){
+            Log.e(TAG,e.toString());
+        }
+        return mRgba;
 
 
     }
