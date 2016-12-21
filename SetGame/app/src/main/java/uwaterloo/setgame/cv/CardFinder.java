1025bf5b4401f44a0ae667dca62cce968e92c587
Stephen Cholvat <stephen.cholvat@gmail.com>
2016-12-20 20:33:31 -0500

    Added more to pseudocode

--- SetGame/app/src/main/java/uwaterloo/setgame/cv/CardFinder.java
+++ SetGame/app/src/main/java/uwaterloo/setgame/cv/CardFinder.java
@@ -1,27 +1,66 @@
 package uwaterloo.setgame.cv;
 
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+
+import java.util.List;
+import java.util.Map;
 
 import uwaterloo.setgame.util.Card;
+import uwaterloo.setgame.util.cardfeatures.Color;
+import uwaterloo.setgame.util.cardfeatures.Fill;
+import uwaterloo.setgame.util.cardfeatures.Shape;
 
 /**
  * Created by Stephen on 12/20/2016.
  */
 public class CardFinder {
+    Card card;
 
     public CardFinder(){
-
+        card = new Card(null,null,null,0);
     }
 
     public Card findCard(Mat img){
-        //Convert to Grayscale
-        //Blur
-        //Canny
-        //Find Contours
-        //Find Rectangles
+        //apply filters
+        //findRect
         //Refilter Rectangle
         //Verify Card
         //Detect Card attributes
         //Return Card
+        return card;
     }
+
+    private Mat applyFilters(Map filters){
+        //Convert to Grayscale
+        //Blur
+        //Canny
+        return null;
+    }
+
+    private List<MatOfPoint> findRect(){
+        //Find Contours
+        //Find Rectangles
+        return null;
+    }
+
+    private Card detectAttributes(){
+        return null;
+    }
+    private int detectNumber(){
+        return -1;
+    }
+
+    private Shape detectShape(){
+        return null;
+    }
+
+    private Color detectColor(){
+        return null;
+    }
+
+    private Fill detectFill(){
+        return null;
+    }
+
 }
